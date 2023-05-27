f4344c665a72eef1058fcbd159e851c03d552d47
phamminhtnl <contact.animehay@gmail.com>
2023-05-27 12:45:59 +0700

    init

---
+++ js/config.js
@@ -0,0 +1,52 @@
+const USER = {
+    banks: [{
+            name: "Vietinbank",
+            logo: "./images/vietinbank.jpg",
+            number: "103870584295",
+            owner: "PHAM NGOC MINH"
+        },
+        {
+            name: "MOMO",
+            logo: "./images/momo.jpeg",
+            number: "0912021458",
+            owner: "PHAM NGOC MINH",
+            fastTransfer: true
+        },
+        // {
+        //     name: "BIDV BANK",
+        //     logo: "./images/bidv.png",
+        //     number: "42710000636335",
+        //     owner: "LE ANH DUC"
+        // },
+        // {
+        //     name: "AGRI BANK",
+        //     logo: "./images/agribank.png",
+        //     number: "xxxxxxxxxxxx",
+        //     owner: "LE ANH DUC"
+        // },
+        // {
+        //     name: "PAYPAL",
+        //     logo: "./images/paypal.png",
+        //     number: "leanhducprovn",
+        //     owner: "LE ANH DUC"
+        // },
+        // {
+        //     name: "ZALO PAY",
+        //     logo: "./images/zalopay.png",
+        //     number: "0386897757",
+        //     owner: "LE ANH DUC"
+        // },
+        // {
+        //     name: "VIETTEL PAY",
+        //     logo: "./images/viettelpay.png",
+        //     number: "0386897757",
+        //     owner: "LE ANH DUC"
+        // },
+        // {
+        //     name: "SHOPEE PAY",
+        //     logo: "./images/shopeepay.png",
+        //     number: "xxxxxxxxxxxx",
+        //     owner: "LE ANH DUC"
+        // },
+    ]
+};
